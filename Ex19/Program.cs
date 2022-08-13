a5f739ded4591dd553c3e87236fde0430d78c019
DmitriySolenkov <soledimk@gmail.com>
2022-08-13 13:04:01 +0300

    Ex19

---
+++ Ex19/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 9999 && num <= 99999)
+{
+    int num1 = num / 10000;
+    int num2 = (num / 1000) % 10;
+    int num4 = (num / 10) % 10;
+    int num5 = num % 10;
+    if (num1==num5 && num2==num4) Console.WriteLine("Число является палиндромом");
+    else Console.WriteLine("Число НЕ является палиндромом");
+}
+else Console.WriteLine("Число не пятизначное!");
