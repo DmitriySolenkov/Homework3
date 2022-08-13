13817a1dff40abed1a5efd6998d86d9aaa932124
DmitriySolenkov <soledimk@gmail.com>
2022-08-13 13:34:50 +0300

    Ex23

---
+++ Ex23/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите число, отличное от 0: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+if (N > 0)
+{
+    while (count < N)
+    {
+        count++;
+        Console.Write(Math.Pow(count, 3) + ", ");
+    }
+}
+else
+{
+   if (N < 0)
+{
+    while (count > N)
+    {
+        count= count-1;
+        Console.Write(Math.Pow(count, 3) + ", ");
+    }
+} 
+else Console.WriteLine("Число некорректно!");
+}
